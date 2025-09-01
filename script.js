1676c6fef5b2f4d9c2ccee4ee82e5a94cf5b4ce3
Grigioo <8grigioo@gmail.com>
2025-09-01 21:51:24 +0300

    Edit script.js. Add logic for 1 round of game.

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const scissors = "scissors";
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
 
 function getComputerChoice() {
     // Variables for number that represents choice and future string choice.
@@ -33,4 +35,47 @@ function getHumanChoice() {
     // Input human choice.
     let humanChoice = prompt("What is your choice? ");
     return humanChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    // Make human choice dosen't depend on case.
+    humanChoice = humanChoice.toLowerCase();
+    let result = "";
+
+    // Check one by one conditions for victory, draw and loose.
+    if(humanChoice == "rock" && computerChoice == "scissors") {
+        result = "You win!";
+        humanScore++;
+    }
+    else if(humanChoice == "rock" && computerChoice == "rock")
+        result = "Draw.";
+    else if(humanChoice == "rock" && computerChoice == "paper") {
+        result = "You lose...";
+        computerScore++;
+    }
+
+    if(humanChoice == "paper" && computerChoice == "rock") {
+        result = "You win!";
+        humanScore++;
+    }
+    else if(humanChoice == "paper" && computerChoice == "paper")
+        result = "Draw.";
+    else if(humanChoice == "paper" && computerChoice == "scissors") {
+        result = "You lose...";
+        computerScore++;
+    }
+
+    if(humanChoice == "scissors" && computerChoice == "paper") {
+        result = "You win!";
+        humanScore++;
+    }
+    else if(humanChoice == "scissors" && computerChoice == "scissors")
+        result = "Draw.";
+    else if(humanChoice == "scissors" && computerChoice == "rock") {
+        result = "You lose...";
+        computerScore++;
+    }
+
+    // console.log("Human choice: " + humanChoice + ", computer choice: " + computerChoice + ". Human score: " + humanScore + ", computer score: " + computerScore);
+    return result;
 }
