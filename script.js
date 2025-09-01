38a07072b7f2cc84a79ad45ff4ea592eeaf56706
Grigioo <8grigioo@gmail.com>
2025-09-01 21:30:12 +0300

    Edit script.js. Add comments.

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ const scissors = "scissors";
 getComputerChoice();
 
 function getComputerChoice() {
+    // Variables for number that represents choice and future string choice.
     let choiceNumber = Math.floor(Math.random() * 3);
     let computerChoice = "";
 
+    // Choosing value for computer choice.
     switch (choiceNumber) {
         case 0:
             computerChoice = rock;
