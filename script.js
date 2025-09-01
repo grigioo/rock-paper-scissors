7df4cdaa2bb212f040f88f35d45ad0d17e9246ca
Grigioo <8grigioo@gmail.com>
2025-09-01 21:35:11 +0300

    Edit script.js. Add logic for human input.

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Global constants and variables.
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
+let humanScore = 0;
+let computerScore = 0;
 
-getComputerChoice();
+console.log(getComputerChoice());
+console.log(getHumanChoice());
 
 function getComputerChoice() {
     // Variables for number that represents choice and future string choice.
@@ -23,4 +27,10 @@ function getComputerChoice() {
     }
 
     return computerChoice;
+}
+
+function getHumanChoice() {
+    // Input human choice.
+    let humanChoice = prompt("What is your choice? ");
+    return humanChoice;
 }
